a9e51ff413d08d711de6f2c4d99965306c59433a
traveler-pinkie <ja963mes@gmail.com>
2023-02-22 07:00:57 -0500

    these are the changes for issue 2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(123);
+//creating express server
+const express = require('express');
+//calling the .env environment
+const dotenv = require('dotenv').config();
+//getting port value from .env or setting to 5000
+const port = process.env.PORT || 5000;
+//itializing express
+const app = express();
+
+//passing port to test server
+app.listen(port, ()=> console.log(`Server started on port ${port}`));

ce8f630d4195a926204417a71d1513436bdb6ad2
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2023-02-23 10:40:46 -0500

    test issue

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 //creating express server
-const express = require('express');
+const express = require("express");
 //calling the .env environment
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 //getting port value from .env or setting to 5000
 const port = process.env.PORT || 5000;
 //itializing express
 const app = express();
 
 //passing port to test server
-app.listen(port, ()=> console.log(`Server started on port ${port}`));
+// testing comment
+// x2
+app.listen(port, () => console.log(`Server started on port ${port}`));

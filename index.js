a3f4b0b56e52435832076129e2b61e7bf23ef30b
briansegura15 <84042284+briansegura15@users.noreply.github.com>
2023-02-23 10:55:37 -0500

    test #2

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 //passing port to test server
-// testing comment
-// x2
+
 app.listen(port, () => console.log(`Server started on port ${port}`));

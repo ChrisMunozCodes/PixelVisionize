1cbccdc9014ecf7bd44c288ae6227c136cf71edd
traveler-pinkie <ja963mes@gmail.com>
2023-02-26 21:22:00 -0500

    code for generate-image-request

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,39 @@ function onsubmit(e){
     generateImageRequest(prompt, size);
 }
 
+async function generateImageRequest(prompt, size){
+    try{
+        showSpinner();
+
+        const response = await fetch('/openai/generateimage', {
+        method: 'POST',
+        headers: {
+                'Content-Type': 'application/json'
+            },
+        body: JSON.stringify({
+            prompt,
+            size
+        })
+    });
+    if(!response.ok){
+        removeSpinner();
+        throw new Error('That image could not be generated')
+    }
+
+    const data = await response.json();
+    removeSpinner();
+    }catch(error){
+        document.querySelector('.msg').textContent = error;
+    }
+}
+
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function removeSpinner(){
+    document.querySelector('.spinner').classList.remove('show');
+}
 
 document.querySelector('#image-form').addEventListener('submit', onsubmit);
 
